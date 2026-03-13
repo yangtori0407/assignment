13125e76388e912586633b1e0c33bcc5ad922d02
yangtori0407 <heyang236@gmail.com>
2026-03-13 23:01:54 +0900

    baseball js 완성

--- baseball/baseball.js
+++ baseball/baseball.js
@@ -8,15 +8,35 @@
 <td class="dis">0.0</td>
 <td class="seqLose">3패</td> */
 
-let body = document.getElementsByTagName("tbody");
+let body = document.getElementById("tbody");
+let html = "";
 fetch("https://prof-jwchi.github.io/kopo-front2026/Json/baseball.json")
 .then(response =>{
     return response.json()
 }).then(data =>{
 
-    for(let i = 1;i <= 10; i++){
-        let tr = document.createElement("tr");
+    for(let i = 0;i < 10; i++){
 
-        for()
+        let tr = document.createElement("tr");
+        let str = ""
+    
+        str += `<td class="grade" style="padding: 0px; text-align: center">${i + 1}</td>
+                <td class="name">
+                    <div>
+                        <img src="${data.list[i].imageUrl}" alt="">
+                        <span>${data.list[i].shortName}</span>
+                    </div>
+                </td>
+                <td class="round">${data.list[i].rank.game}</td>
+                <td class="win">${data.list[i].rank.win}</td>
+                <td class="compare">${data.list[i].rank.draw}</td>
+                <td class="lose">${data.list[i].rank.loss}</td>
+                <td class="winRate">${data.list[i].rank.wpct}</td>
+                <td class="dis">${data.list[i].rank.gb}</td>
+                <td class="seqLose">${data.list[i].rank.streak}</td>`
+        
+        tr.innerHTML = str;
+
+        body.appendChild(tr);
     }
 })
